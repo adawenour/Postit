a3dc157512996ea3e140ab80bc7030caf0fa617d
Adawe Nour <nouradawe@gmail.com>
2014-07-03 07:14:51 -0400

    Completing the rest of the Users actions: editing and showing user functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+	before_action :require_same_user, only: [:edit, :update]
+
+	def show 
+	end
 
 	def new 
 		@user = User.new
+	end
+
+	def edit 
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Your profile was update!"
+			redirect_to user_path
+		else 
+			render :edit
+		end
+
 	end
 
 	def create
@@ -21,5 +39,16 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:username, :password)
 	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:error] = "You're not allowed to do that."
+			redirect_to root_path
+	end
+end
 
 end
