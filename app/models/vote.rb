b7043e351be67dd86741ab23c8ad59e7d7ecdaae
Adawe Nour <nouradawe@gmail.com>
2014-07-07 23:44:48 -0400

    modified votes code

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,8 @@ class Vote < ActiveRecord::Base
 	belongs_to :voteable, polymorphic: true
 
 	validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
+	
+
 	# validates_uniqueness_of :user, scope: :voteable
+	# syntax not allowing to vote on both objects of the same id, you can either vote on post or comments not both.
 end
