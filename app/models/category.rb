4f24c66e404d93945d98de2e1ddffa9b7e92bc8e
Adawe Nour <nouradawe@gmail.com>
2014-07-15 10:43:49 -0400

    Added slugs to Users and Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,15 @@ class Category < ActiveRecord::Base
 	has_many :posts, through: :post_categories
 
 	validates :name, presence: true
+
+	#required for category slug
+	before_save :generate_slug
+
+	def to_param
+		self.slug
+	end
+
+	def generate_slug
+		self.slug = self.name.gsub(" ", "-").downcase
+	end
 end
