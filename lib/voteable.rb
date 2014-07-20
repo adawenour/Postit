d7a41a258341b4979ade7eeaebccc416598903e1
Adawe Nour <nouradawe@gmail.com>
2014-07-20 10:38:52 -0400

    Create, build and publish first gem into the wild, then consume it in Postit

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,25 +1,3 @@
-# Using rails Concerns 
-module Voteable
-	extend ActiveSupport::Concern
-
-		included do
-			has_many :votes, as: :voteable
-		end
-
-		def total_votes
-			self.up_votes - self.down_votes #equals totals votes
-		end
-
-		def up_votes
-			self.votes.where(vote: true).size
-		end
-
-		def down_votes
-			self.votes.where(vote: false).size
-		end
-end
-
-
 
 
 # Using the normal metaprogramming way
